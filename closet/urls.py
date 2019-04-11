75f0eced70a19976de026ef225d5e8afd881a954
yezuoxian <yezuoxian@foxmail.com>
2019-04-11 16:04:35 +0800

    Complete product's urls & views

--- closet/urls.py
+++ closet/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.ClosetListView.as_view()),
     path('user_profile/', views.user_profile_list),
     path('user_profile/<int:pk>/', views.user_profile_detail),
+    path('product/', views.product_list),
+    path('product/<int:pk>/', views.product_detail),
 ]
--- closet/views.py
+++ closet/views.py
@@ -5,8 +5,8 @@ from rest_framework.parsers import JSONParser
 
 from . import models
 from . import serializers
-from .models import UserProfile
-from .serializers import UserProfileSerializer
+from .models import UserProfile, Product
+from .serializers import UserProfileSerializer, ProductSerializer
 
 
 # Create your views here.
@@ -58,4 +58,50 @@ def user_profile_detail(request, pk):
 
     elif request.method == 'DELETE':
         user_profile.delete()
+        return HttpResponse(status=204)
+
+
+@csrf_exempt
+def product_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        product = Product.objects.all()
+        serializer = ProductSerializer(product, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ProductSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def product_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        product = Product.objects.get(pk=pk)
+    except UserProfile.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = ProductSerializer(product, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        product.delete()
         return HttpResponse(status=204)
